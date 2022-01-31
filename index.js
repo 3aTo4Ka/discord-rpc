5d4724377b58c130d0763932131a7fde7f7fb652
3aTo4Ka <44292103+3aTo4Ka@users.noreply.github.com>
2022-01-31 18:24:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ client.on('ready', () => {
             'assets': {
                 'large_image': 'top',
                 'large_text': '<image_key>',
-				'small_image': 'small', 
+		'small_image': 'small', 
                 'small_text': '<image_key>'
             },
             'buttons': [
                 {
                     'label': 'VK',
                     'url': 'https://vk.com/3ato4'
-                },{
+                },
+		{
                     'label': 'Discord Bot',
                     'url': 'http://haticocoin.xyz/'
                 }
